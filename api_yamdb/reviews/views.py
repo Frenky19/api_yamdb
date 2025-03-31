073792cc4947fa16b288d8d8b9b86659644923cf
Khamzaev <khamzaew.j@gmail.com>
2025-03-31 20:31:37 +0300

    update reviews/views

--- api_yamdb/reviews/views.py
+++ api_yamdb/reviews/views.py
@@ -1,14 +1,36 @@
 from rest_framework import viewsets
 
-from .models import Review, Comment, Rating
+from .mixins import UpdateRatingMixin
+from .models import Review, Comment
 from api.serializers import CommentSerializer, ReviewSerializer
 
 
-class ReviewViewSet(viewsets.ModelViewSet):
+class ReviewViewSet(UpdateRatingMixin, viewsets.ModelViewSet):
+    """Представление для управления отзывами."""
     queryset = Review.objects.all()
     serializer_class = ReviewSerializer
 
+    def perform_create(self, serializer):
+        """Создание отзыва и обновление рейтинга произведения."""
+        review = serializer.save(user=self.request.user)
+        self.update_title_rating(review.title)
+
+    def perform_update(self, serializer):
+        """Обновление отзыва и рейтинга произведения."""
+        review = serializer.save()
+        self.update_title_rating(review.title)
+
+    def perform_destroy(self, instance):
+        """
+        Удаление отзыва и обновление рейтинга произведения.
+        """
+        title = instance.title
+        instance.delete()
+        self.update_title_rating(title)
+
+
 class CommentViewSet(viewsets.ModelViewSet):
+    """Представление для управления комментариями к отзывам."""
     queryset = Comment.objects.all()
     serializer_class = CommentSerializer
 
