509230c6134e7865d9739e7cc75c87632ee29e44
Andrey Golovushkin <102225757+Frenky19@users.noreply.github.com>
2025-04-01 12:25:23 +0300

    маршруты для авторизации и аутентификации

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -3,13 +3,12 @@ from rest_framework.routers import DefaultRouter
 
 from api.views import CategoryViewSet, TitleViewSet
 from reviews.views import ReviewViewSet, CommentViewSet
-from users.views import UserViewSet
+from users.views import UserView
 
 
 router_v1 = DefaultRouter()
 router_v1.register('categories', CategoryViewSet, basename='categories')
 router_v1.register('titles', TitleViewSet, basename='titles')
-router_v1.register('users', UserViewSet, basename='users')
 router_v1.register(
     r'^titles/(?P<title_id>\d+)/reviews', ReviewViewSet, basename='reviews'
 )
@@ -19,5 +18,6 @@ router_v1.register(
 )
 urlpatterns = [
     path('', include(router_v1.urls)),
-    # path('auth/', include('users.urls')),
+    path('auth/', include('users.urls')),
+    path('users/me/', UserView.as_view(), name='me'),
 ]
