a64b9ceec679e5d57595fdeaa03e04602b7d1b97
Andrey Golovushkin <102225757+Frenky19@users.noreply.github.com>
2025-04-01 12:27:30 +0300

    логика работы с пользователями

--- api_yamdb/users/views.py
+++ api_yamdb/users/views.py
@@ -1,24 +1,37 @@
-from rest_framework.viewsets import ModelViewSet
+from rest_framework.generics import RetrieveUpdateAPIView
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework import status
-from users.models import User
-from api.serializers import UserSerializer
+from rest_framework import status, permissions
+from users.serializers import UserSerializer, SignupSerializer, TokenSerializer
 
 
-class UserViewSet(ModelViewSet):
-    queryset = User.objects.all()
+class UserView(RetrieveUpdateAPIView):
     serializer_class = UserSerializer
-    lookup_field = 'username'
+
+    def get_object(self):
+        return self.request.user
+
+    def perform_update(self, serializer):
+        if 'role' in serializer.validated_data:
+            if not self.request.user.is_admin:
+                del serializer.validated_data['role']
+        serializer.save()
 
 
 class SignupView(APIView):
+    permission_classes = (permissions.AllowAny,)
+
     def post(self, request):
-        # Логика регистрации и отправки confirmation code
-        ...
+        serializer = SignupSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_200_OK)
 
 
 class TokenView(APIView):
+    permission_classes = (permissions.AllowAny,)
+
     def post(self, request):
-        # Логика выдачи JWT токена
-        ...
+        serializer = TokenSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        return Response(serializer.validated_data, status=status.HTTP_200_OK)
