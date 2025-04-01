39ebf902e0ce519cc4b810995589a86560ca1639
Andrey Golovushkin <102225757+Frenky19@users.noreply.github.com>
2025-04-01 17:07:40 +0300

    обновленная модель пользователя

--- api_yamdb/users/models.py
+++ api_yamdb/users/models.py
@@ -1,34 +1,53 @@
 from django.contrib.auth.models import AbstractUser
 from django.core.validators import RegexValidator
 from django.db import models
+from django.utils.text import Truncator
+from users.constants import LIMIT_OF_SYMBOLS
 
 
 class User(AbstractUser):
-    """
-    Кастомная модель пользователя.
+    """Модель пользователя с расширенными полями и ролями."""
 
-
-    """
-
-    ROLES = (
-        ('user', 'User'),
-        ('moderator', 'Moderator'),
-        ('admin', 'Admin'),
+    USER = 'user'
+    MODERATOR = 'moderator'
+    ADMIN = 'admin'
+    ROLE_CHOICES = (
+        (USER, 'Пользователь'),
+        (MODERATOR, 'Модератор'),
+        (ADMIN, 'Администратор'),
     )
     username = models.CharField(
+        'Логин',
         max_length=150,
         unique=True,
         validators=[
             RegexValidator(
                 regex=r'^[\w.@+-]+\Z',
-                message='Недопустимые символы в имени пользователя'
+                message='Недопустимые символы в имени пользователя.'
             )
         ]
     )
-    email = models.EmailField(max_length=254, unique=True)
-    bio = models.TextField(blank=True)
-    role = models.CharField(max_length=20, choices=ROLES, default='user')
-    confirmation_code = models.CharField(max_length=6, blank=True)
+    email = models.EmailField('Электронная почта', max_length=254, unique=True)
+    bio = models.TextField('Биография', blank=True)
+    role = models.CharField(
+        'Роль',
+        max_length=100,
+        choices=ROLE_CHOICES,
+        default='user'
+    )
+    confirmation_code = models.CharField(
+        'Код подтверждения',
+        max_length=16,
+        blank=True
+    )
+
+    class Meta:
+        ordering = ['username']
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
+
+    def __str__(self):
+        return Truncator(self.username).words(LIMIT_OF_SYMBOLS)
 
     @property
     def is_admin(self):
