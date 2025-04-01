ede552c692a1b2b85b354366e71633c723f2e17f
Andrey Golovushkin <102225757+Frenky19@users.noreply.github.com>
2025-04-01 12:23:55 +0300

    Тесты сказали, что модели должны хранится в приложении reviews

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,8 +1,31 @@
 from django.db import models
 from django.core.validators import MinValueValidator, MaxValueValidator
 
-from api.models import Title
 from users.models import User
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=256)
+    slug = models.SlugField(max_length=50, unique=True)
+
+
+class Genre(models.Model):
+    name = models.CharField(max_length=256)
+    slug = models.SlugField(max_length=50, unique=True)
+
+
+class Title(models.Model):
+    name = models.CharField(max_length=256)
+    year = models.IntegerField()
+    description = models.TextField()
+    genre = models.ManyToManyField(Genre)
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.SET_NULL,
+        null=True
+    )
+    # Поле рейтинга (вотчина Джамала:)
+    rating = models.IntegerField(null=True)
 
 
 class Review(models.Model):
