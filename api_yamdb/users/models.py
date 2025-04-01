61c870fd5a21328f9d3f99b00c052cac9ed70c72
Andrey Golovushkin <102225757+Frenky19@users.noreply.github.com>
2025-04-01 12:27:44 +0300

    модель пользователя

--- api_yamdb/users/models.py
+++ api_yamdb/users/models.py
@@ -1,14 +1,39 @@
 from django.contrib.auth.models import AbstractUser
+from django.core.validators import RegexValidator
 from django.db import models
 
 
 class User(AbstractUser):
+    """
+    Кастомная модель пользователя.
+
+
+    """
+
     ROLES = (
         ('user', 'User'),
         ('moderator', 'Moderator'),
         ('admin', 'Admin'),
     )
+    username = models.CharField(
+        max_length=150,
+        unique=True,
+        validators=[
+            RegexValidator(
+                regex=r'^[\w.@+-]+\Z',
+                message='Недопустимые символы в имени пользователя'
+            )
+        ]
+    )
+    email = models.EmailField(max_length=254, unique=True)
     bio = models.TextField(blank=True)
     role = models.CharField(max_length=20, choices=ROLES, default='user')
-    email = models.EmailField(unique=True)
-    confirmation_code = models.CharField(max_length=100, blank=True)
+    confirmation_code = models.CharField(max_length=6, blank=True)
+
+    @property
+    def is_admin(self):
+        return self.role == 'admin' or self.is_superuser
+
+    @property
+    def is_moderator(self):
+        return self.role == 'moderator'
