b229957f48ff420286399335f4b5927b4d992e90
Andrey Golovushkin <102225757+Frenky19@users.noreply.github.com>
2025-04-02 14:32:34 +0300

    Готовые маршруты для регистарции пользователя, получения кода подтверждения и JWT токена

--- api_yamdb/users/urls.py
+++ api_yamdb/users/urls.py
@@ -3,19 +3,6 @@ from django.urls import path
 from users.views import SignupView, TokenObtainView
 
 urlpatterns = [
-    path(
-        'signup/',
-        SignupView.as_view(),
-        name='signup',
-        doc=(
-            'Регистрация нового пользователя/'
-            'получение кода подтверждения для зарегестрированного'
-        )
-    ),
-    path(
-        'token/',
-        TokenObtainView.as_view(),
-        name='token_obtain',
-        doc="Получение JWT токена"
-    ),
+    path('signup/', SignupView.as_view(), name='signup'),
+    path('token/', TokenObtainView.as_view(), name='token_obtain'),
 ]
