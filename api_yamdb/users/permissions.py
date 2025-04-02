61dda0e5d3599bb0dd0b878b7ded6402978545bc
Andrey Golovushkin <102225757+Frenky19@users.noreply.github.com>
2025-04-02 14:19:47 +0300

    Готовые разрешения/права для пользователей

--- api_yamdb/users/permissions.py
+++ api_yamdb/users/permissions.py
@@ -9,37 +9,3 @@ class IsAdmin(permissions.BasePermission):
             request.user.is_authenticated
             and request.user.is_admin
         )
-
-
-class IsAdminOrReadOnly(permissions.BasePermission):
-    """
-    Изменения только для администратора.
-
-    Разрешает безопасные методы для всех пользователей.
-    """
-
-    def has_permission(self, request, view):
-        return (
-            request.method in permissions.SAFE_METHODS
-            or request.user.is_authenticated
-            and request.user.is_admin
-        )
-
-
-class IsAdminModeratorOwnerOrReadOnly(permissions.BasePermission):
-    """
-    Изменения доступны владельцу, модератору или администратору.
-
-    Разрешает безопасные методы для всех пользователей.
-    """
-
-    def has_object_permission(self, request, view, obj):
-        return (
-            request.method in permissions.SAFE_METHODS
-            or request.user.is_authenticated
-            and (
-                request.user.is_admin
-                or request.user.is_moderator
-                or obj.author == request.user
-            )
-        )
