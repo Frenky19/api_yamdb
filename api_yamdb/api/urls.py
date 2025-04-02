c66f870768520f4c3c28105edea83f66ba088b8c
Andrey Golovushkin <102225757+Frenky19@users.noreply.github.com>
2025-04-02 14:36:37 +0300

    Добавил маршруты для жанров

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -1,7 +1,7 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
-from api.views import CategoryViewSet, TitleViewSet
+from api.views import CategoryViewSet, TitleViewSet, GenreViewSet
 from reviews.views import ReviewViewSet, CommentViewSet
 from users.views import UserViewSet
 
@@ -10,6 +10,7 @@ router_v1 = DefaultRouter()
 router_v1.register('users', UserViewSet, basename='users')
 router_v1.register('categories', CategoryViewSet, basename='categories')
 router_v1.register('titles', TitleViewSet, basename='titles')
+router_v1.register('genres', GenreViewSet, basename='genres')
 router_v1.register(
     r'^titles/(?P<title_id>\d+)/reviews', ReviewViewSet, basename='reviews'
 )
