c285da53e0144c9d075dd47a5fd4f5d58580fcf7
Andrey Golovushkin <102225757+Frenky19@users.noreply.github.com>
2025-04-02 13:02:22 +0300

    Готовые сериализаторы для пользователей

--- api_yamdb/users/serializers.py
+++ api_yamdb/users/serializers.py
@@ -1,13 +1,18 @@
-from rest_framework import serializers
-from rest_framework.validators import UniqueValidator
 from django.core.validators import RegexValidator
 from django.shortcuts import get_object_or_404
-from users.models import User
+from rest_framework import serializers
+from rest_framework.validators import UniqueValidator
 from rest_framework_simplejwt.tokens import AccessToken
+
+from users.models import User
 
 
 class UserSerializer(serializers.ModelSerializer):
-    """Сериализатор для модели пользователя."""
+    """Сериализатор для модели User.
+
+    Используется для сериализации данных пользователя, включая поля:
+    username, email, first_name, last_name, bio и role.
+    """
 
     class Meta:
         model = User
@@ -18,16 +23,28 @@ class UserSerializer(serializers.ModelSerializer):
 
 
 class SignupSerializer(serializers.ModelSerializer):
-    """Сериализатор для регистрации пользователя."""
+    """Сериализатор для регистрации нового пользователя.
+
+    Выполняет валидацию полей email и username:
+    - email должен быть уникальным;
+    - username должен быть уникальным, соответствовать шаблону и не быть
+        равным 'me'.
+    """
 
     email = serializers.EmailField(
         max_length=254,
-        validators=[UniqueValidator(queryset=User.objects.all())]
+        validators=[UniqueValidator(
+            queryset=User.objects.all(),
+            message='Пользователь с таким email уже существует.'
+        )]
     )
     username = serializers.CharField(
         max_length=150,
         validators=[
-            UniqueValidator(queryset=User.objects.all()),
+            UniqueValidator(
+                queryset=User.objects.all(),
+                message='Пользователь с таким username уже существует.'
+            ),
             RegexValidator(regex=r'^[\w.@+-]+\Z')
         ]
     )
@@ -45,16 +62,26 @@ class SignupSerializer(serializers.ModelSerializer):
 
 
 class TokenSerializer(serializers.Serializer):
-    """Сериализатор для получения токена."""
+    """Сериализатор для получения токена аутентификации.
+
+    Требует поля username и confirmation_code. Проверяет, что confirmation_code
+    соответствует коду, хранящемуся для пользователя, и при успешной валидации
+    возвращает токен и username.
+    """
 
     username = serializers.CharField(max_length=150)
     confirmation_code = serializers.CharField()
 
     def validate(self, data):
         user = get_object_or_404(User, username=data['username'])
-        if user.confirmation_code != data['confirmation_code']:
+        confirmation_code = data.get('confirmation_code')
+        if not confirmation_code:
             raise serializers.ValidationError(
-                'Неверный код подтверждения.'
+                {'confirmation_code': 'Код подтверждения обязателен.'}
+            )
+        if user.confirmation_code != confirmation_code:
+            raise serializers.ValidationError(
+                {'confirmation_code': 'Неверный код подтверждения.'}
             )
         return {
             'token': str(AccessToken.for_user(user)),
@@ -63,7 +90,12 @@ class TokenSerializer(serializers.Serializer):
 
 
 class MeSerializer(serializers.ModelSerializer):
-    """Сериализатор для редактирования профиля пользователя."""
+    """Сериализатор для редактирования профиля аутентифицированного юзера.
+
+    Позволяет просматривать и редактировать поля: username, email, first_name,
+    last_name и bio.
+    Поле role не включено и не может быть изменено через этот сериализатор.
+    """
 
     class Meta:
         model = User
@@ -72,60 +104,3 @@ class MeSerializer(serializers.ModelSerializer):
             'last_name', 'bio'
         )
         read_only_fields = ('role',)
-
-
-
-# from rest_framework import serializers
-# from rest_framework_simplejwt.tokens import AccessToken
-# from django.contrib.auth import authenticate
-# from users.models import User
-# import secrets
-# from django.core.mail import send_mail
-
-
-# class UserSerializer(serializers.ModelSerializer):
-#     class Meta:
-#         model = User
-#         fields = (
-#             'username', 'email', 'first_name', 'last_name', 'bio', 'role'
-#         )
-
-
-# class SignupSerializer(serializers.ModelSerializer):
-#     class Meta:
-#         model = User
-#         fields = ('email', 'username')
-
-#     def validate_username(self, value):
-#         if value.lower() == 'me':
-#             raise serializers.ValidationError('Имя "me" запрещено')
-#         return value
-
-#     def create(self, validated_data):
-#         user = User.objects.create(**validated_data)
-#         user.confirmation_code = secrets.token_urlsafe(16)
-#         user.save()
-#         send_mail(
-#             subject='Код подтверждения Yamdb',
-#             message=f'Ваш код подтверждения: {user.confirmation_code}',
-#             from_email=None,  # Значение берется из DEFAULT_FROM_EMAIL
-#             recipient_list=[user.email],
-#             fail_silently=False,
-#         )
-#         return user
-
-
-# class TokenSerializer(serializers.Serializer):
-#     username = serializers.CharField()
-#     confirmation_code = serializers.CharField()
-
-#     def validate(self, data):
-#         user = User.objects.filter(username=data['username']).first()
-
-#         if not user or user.confirmation_code != data['confirmation_code']:
-#             raise serializers.ValidationError(
-#                 'Введен неверный проверочный код или имя пользователя.'
-#             )
-#         return {
-#             'token': str(AccessToken.for_user(user))
-#         }
