0127908ae6fb1c53dd80f4507f17947f2f367102
Andrey Golovushkin <102225757+Frenky19@users.noreply.github.com>
2025-04-03 12:51:36 +0300

    +

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -25,5 +25,5 @@ router_v1.register(
 )
 urlpatterns = [
     path('', include(router_v1.urls)),
-    path('auth/', include('users.urls')),
+    path('auth/', include('users.urls'), name='auth'),
 ]
