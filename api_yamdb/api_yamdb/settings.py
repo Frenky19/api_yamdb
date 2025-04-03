a254b9338e6c0435711fed796252b2aec295d271
Andrey Golovushkin <102225757+Frenky19@users.noreply.github.com>
2025-04-03 11:16:35 +0300

    добавил 'django_filters'

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -21,6 +21,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'rest_framework_simplejwt',
+    'django_filters',
     'api.apps.ApiConfig',
     'users.apps.UsersConfig',
     'reviews.apps.ReviewsConfig',
