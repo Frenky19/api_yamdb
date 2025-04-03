9ce648a62d74058dd880ad46331a76769ec8e7a5
Andrey Golovushkin <102225757+Frenky19@users.noreply.github.com>
2025-04-03 11:16:03 +0300

    Обновленные представления для категорий, жанров, произведений, комментов и отзывов

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,53 +1,98 @@
+from django.db.models import Avg
+from rest_framework import viewsets
+from django.shortcuts import get_object_or_404
+from rest_framework.filters import SearchFilter
 from rest_framework.viewsets import ModelViewSet
+from django_filters.rest_framework import DjangoFilterBackend
 
-from reviews.models import Category, Genre, Title
+from api.filters import TitleFilter
+from api.mixins import ModelMixinSet, PUTNotAllowedMixin
 from api.serializers import (
-    CategorySerializer,
-    GenreSerializer,
-    TitleSerializer,
+    CategorySerializer, CommentSerializer,
+    ReviewSerializer, TitleReadSerializer,
+    TitleWriteSerializer, GenreSerializer,
 )
-from users.permissions import IsAdmin
-#from api.filters import TitleFilter
+from reviews.models import Category, Genre, Title, Review
+from reviews.permissions import (
+    IsAdminUserOrReadOnly,
+    AdminModeratorAuthorPermission
+)
 
 
-class CategoryViewSet(ModelViewSet):
-    """Представление для управления категориями."""
+class CategoryViewSet(ModelMixinSet):
+    """
+    Получить список всех категорий. Права доступа: Доступно без токена
+    """
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
-    permission_classes = [IsAdmin]
+    permission_classes = (IsAdminUserOrReadOnly,)
+    filter_backends = (SearchFilter, )
+    search_fields = ('name', )
     lookup_field = 'slug'
-    #filter_backends = [filters.SearchFilter]
-    #search_fields = ['name'] #чуть позже допишу файл фильтр 
-
-    def perform_create(self, serializer):
-        serializer.save()
 
 
-class GenreViewSet(ModelViewSet):
-    """Представление для управления жанрами."""
-
+class GenreViewSet(ModelMixinSet):
+    """
+    Получить список всех жанров. Права доступа: Доступно без токена
+    """
     queryset = Genre.objects.all()
     serializer_class = GenreSerializer
-    permission_classes = [IsAdmin]
+    permission_classes = (IsAdminUserOrReadOnly,)
+    filter_backends = (SearchFilter,)
+    search_fields = ('name', )
     lookup_field = 'slug'
-    #filter_backends = [filters.SearchFilter]
-    #search_fields = ['name']
+
+
+class TitleViewSet(PUTNotAllowedMixin, ModelViewSet):
+    """
+    Получить список всех объектов. Права доступа: Доступно без токена
+    """
+    queryset = Title.objects.annotate(
+        rating=Avg('reviews__score')
+    ).all()
+    permission_classes = (IsAdminUserOrReadOnly,)
+    filter_backends = (DjangoFilterBackend, )
+    filterset_class = TitleFilter
+
+    def get_serializer_class(self):
+        if self.action in ('list', 'retrieve'):
+            return TitleReadSerializer
+        return TitleWriteSerializer
+
+
+class ReviewViewSet(PUTNotAllowedMixin, viewsets.ModelViewSet):
+    """Представление для управления отзывами."""
+
+    serializer_class = ReviewSerializer
+    permission_classes = (AdminModeratorAuthorPermission,)
+
+    def get_queryset(self):
+        title = get_object_or_404(
+            Title,
+            id=self.kwargs.get('title_id'))
+        return title.reviews.all()
 
     def perform_create(self, serializer):
-        serializer.save()
+        title = get_object_or_404(
+            Title,
+            id=self.kwargs.get('title_id'))
+        serializer.save(author=self.request.user, title=title)
 
 
-class TitleViewSet(ModelViewSet):
-    """Представление для управления произведениями."""
+class CommentViewSet(PUTNotAllowedMixin, viewsets.ModelViewSet):
+    """Представление для управления комментариями к отзывам."""
 
-    queryset = Title.objects.all()
-    serializer_class = TitleSerializer
-    permission_classes = [IsAdmin]
-    #filter_backends = [DjangoFilterBackend]
-    #filterset_class = TitleFilter
+    serializer_class = CommentSerializer
+    permission_classes = (AdminModeratorAuthorPermission,)
+
+    def get_queryset(self):
+        review = get_object_or_404(
+            Review,
+            id=self.kwargs.get('review_id'))
+        return review.comments.all()
 
     def perform_create(self, serializer):
-        serializer.save()
-
-    def perform_update(self, serializer):
-        serializer.save()  
+        review = get_object_or_404(
+            Review,
+            id=self.kwargs.get('review_id'))
+        serializer.save(author=self.request.user, review=review)
