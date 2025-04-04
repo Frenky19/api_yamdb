0efadfc8232ab56185c5e0dc29edc22cd3eb1cfe
Andrey Golovushkin <102225757+Frenky19@users.noreply.github.com>
2025-04-04 15:39:23 +0300

    Вспомогательные функции

--- api_yamdb/users/service.py
+++ api_yamdb/users/service.py
@@ -1,26 +1,3 @@
-from django.conf import settings
-from django.core.mail import send_mail
-from rest_framework import status
-from rest_framework.response import Response
-
-
-def send_confirmation_email(user, confirmation_code):
-    """Вспомогательная функция для отправки кода подтверждения."""
-    try:
-        send_mail(
-            subject='Код подтверждения',
-            message=f'Ваш код подтверждения: {confirmation_code}',
-            from_email=None,
-            recipient_list=[user.email],
-            fail_silently=False,
-        )
-    except Exception as e:
-        error_message = (
-            str(e)
-            if settings.DEBUG
-            else 'Не удалось отправить email'
-        )
-        return Response(
-            {'error': error_message},
-            status=status.HTTP_500_INTERNAL_SERVER_ERROR
-        )
+def get_max_length(choices):
+    """Вычисляет максимальную длину значений из списка choices."""
+    return max(len(str(choice[0])) for choice in choices) if choices else 1
