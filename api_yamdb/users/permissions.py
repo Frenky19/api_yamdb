47deb17b0cd811bbce14eec7dce23db81788ece0
Andrey Golovushkin <102225757+Frenky19@users.noreply.github.com>
2025-04-04 13:16:58 +0300

    Исправленные пермишены

--- api_yamdb/users/permissions.py
+++ api_yamdb/users/permissions.py
@@ -12,25 +12,52 @@ class IsAdmin(permissions.BasePermission):
 
 
 class AdminModeratorAuthorPermission(permissions.BasePermission):
-    def has_permission(self, request, view):
-        return request.method in permissions.SAFE_METHODS or (
-            request.user.is_authenticated
-        )
+    """
+    Предоставляет доступ в объекту при определенных условиях.
+
+    1. Запрос является безопасным (GET, HEAD, OPTIONS)
+    2. Пользователь - автор объекта
+    3. Пользователь имеет роль администратора
+    4. Пользователь имеет роль модератора
+    """
 
     def has_object_permission(self, request, view, obj):
-        if request.method in permissions.SAFE_METHODS:
-            return True
-        if obj.author == request.user:
-            return True
-        if request.user.is_admin:
-            return True
-        if request.user.is_moderator:
-            return True
-        return False
+        """
+        Проверка разрешений для конкретного объекта.
+
+        Разрешает доступ:
+        - Для чтения данных всем пользователям
+        - Для записи только автору/администратору/модератору
+        """
+        return (
+            request.method in permissions.SAFE_METHODS
+            or obj.author == request.user
+            or request.user.is_admin
+            or request.user.is_moderator
+        )
 
 
 class IsAdminUserOrReadOnly(permissions.BasePermission):
+    """
+    Предоставляет доступ в объекту при определенных условиях.
+
+    1. Запрос использует безопасные методы (GET, HEAD, OPTIONS)
+    2. Пользователь аутентифицирован и имеет статус администратора
+
+    Разрешения:
+    - Чтение: разрешено всем пользователям
+    - Запись: только администраторам
+    """
+
     def has_permission(self, request, view):
-        if request.method in permissions.SAFE_METHODS:
-            return True
-        return request.user.is_authenticated and request.user.is_admin
+        """
+        Проверяет разрешения на уровне всего запроса.
+
+        Разрешает доступ:
+        - Используется безопасный HTTP-метод
+        - ИЛИ пользователь аутентифицирован и является администратором
+        """
+        return (
+            request.method in permissions.SAFE_METHODS
+            or request.user.is_authenticated and request.user.is_admin
+        )
