17cb093d6f9f825f0dc8911ae636839f537d6ad6
Andrey Golovushkin <102225757+Frenky19@users.noreply.github.com>
2025-04-05 19:33:56 +0300

    Переместил все в приложение апи

--- api_yamdb/users/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-
-from users.views import SignupView, TokenObtainView
-
-urlpatterns = [
-    path('signup/', SignupView.as_view(), name='signup'),
-    path('token/', TokenObtainView.as_view(), name='token_obtain'),
-]
